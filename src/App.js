34c3f4f5537efa12b2db688acd70ea7caffd4b48
asvaruda <gdr20021019@gmail.com>
2025-01-13 22:40:33 +0530

    test chnage in app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
-      {/* Hello */}
+      {/* <Navbar />
+      <Home /> */}
+      Hello
     </div>
   );
 }
