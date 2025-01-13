4e36aa268c055bd4f17308f17b7bfe0d2137c21c
asvaruda <gdr20021019@gmail.com>
2025-01-13 23:15:19 +0530

    chages in package.json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
-      {/* <Navbar />
-      <Home /> */}
-      Hello
+      <Home />
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import About from "../../components/About/About";
 import Records from "../../components/Records/Records";
+import Navbar  from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <Carousel />
       <About />
       <Records />
